4d1d15bdc177af301a6f8701d9da85c292136282
Francesco <cesaranofrancescomain@gmail.com>
2022-12-27 11:36:46 +0100

    importato componente NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script >
+import NavBar from './components/NavBar.vue'
+
+
+export default {
+
+  name: "App",
+  components: {
+    NavBar,
+
+
+  }
+
+}
+
 </script>
 
 <template>
+  <!-- header  -->
+  <NavBar />
+
 
 </template>
 
-<style >
-
+<style lang="scss">
+@use "../src/styles/generals.scss";
 </style>
