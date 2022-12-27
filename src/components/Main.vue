dfab06b14edf18cfc606893b407f970091ed27af
Francesco <cesaranofrancescomain@gmail.com>
2022-12-27 19:16:28 +0100

    creat base del componente main

---
+++ src/components/Main.vue
@@ -0,0 +1,37 @@
+<script >
+import { store } from "../store.js";
+import Hero from './hero.vue'
+
+
+export default {
+
+    name: "Main",
+    components: {
+        Hero
+
+
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+
+}
+</script>
+
+<template>
+    <main>
+        <Hero :data="store" />
+    </main>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables.scss" as *;
+
+
+@media all and (max-width: 1250px) {}
+
+
+@media all and (max-width: 900px) {}
+</style>
