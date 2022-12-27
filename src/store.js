0210125daa62a9ed111d324e625e44264010b9d1
Francesco <cesaranofrancescomain@gmail.com>
2022-12-27 19:16:04 +0100

    aggiunte img dinamiche hero

--- src/store.js
+++ src/store.js
@@ -25,5 +25,28 @@ export const store = reactive({
             link: "#",
             icon: "fa-brands fa-facebook-f"
         }
-    ]
+    ],
+    heroImgs: [
+        {
+            id: 0,
+            scr: "../../public/img/hero1.jpg",
+            name: "esterno 1"
+        },
+        {
+            id: 1,
+            scr: "../../public/img/hero2.jpg",
+            name: "esterno 2"
+        },
+        {
+            id: 2,
+            scr: "../../public/img/hero3.jpg",
+            name: "esterno 3"
+        },
+        {
+            id: 3,
+            scr: "../../public/img/hero4.jpg",
+            name: "dettaglio 1"
+        },
+
+    ],
 });
