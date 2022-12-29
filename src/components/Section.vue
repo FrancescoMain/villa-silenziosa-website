3a02742c39fd32ca0ce8afa2d9422a7a0aa94c37
Francesco <cesaranofrancescomain@gmail.com>
2022-12-29 12:03:46 +0100

    modifica contenuti e impaginazione

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,103 +1,91 @@
 <script >
 
 
-
-
-
-
 export default {
 
     name: "Section",
-    
+    props: {
+        data: Object,
+    },
     data() {
         return {
-          image:"https://www.banjakulasi.com/upload/iblock/444/44490b83e05d715f833a6fea6cb76209.jpg",
-          text:" A noble but severely ill horse was wandering for weeks and months in the unexplored countryside and finally dropped from exhaustion. He fell into a deep water-hole with a spouting spring. The water in the spring turned out to be healing, the horse recovered in three days and gained strength as if he had never been sick",
-          author:"Raiko Spasoevich, Tesan"
+            image: "https://www.banjakulasi.com/upload/iblock/444/44490b83e05d715f833a6fea6cb76209.jpg",
+            text: " A noble but severely ill horse was wandering for weeks and months in the unexplored countryside and finally dropped from exhaustion. He fell into a deep water-hole with a spouting spring. The water in the spring turned out to be healing, the horse recovered in three days and gained strength as if he had never been sick",
+            author: "Raiko Spasoevich, Tesan"
         }
     }, methods: {
-       
-     
+
+
     },
     mounted() {
-        
+
     }
 
 }
 </script>
 
 <template>
- <section>
-    <div class="container">
-        <div>
-           <img :src="image" alt="">
+    <section>
+        <div class="container">
+            <div class="container-img">
+                <img :src="data.imgSection" alt="">
+            </div>
+            <div class="title-position">
+                <h2>
+                    MARINA DI <br> CASAL VELINO
+                </h2>
+                <p>
+                    {{ text }}
+                </p>
+            </div>
+
         </div>
-        <div class="title-position">
-           <h2>
-               Legend
-               of Kulasi
-               
-           </h2>
-           <p>
-            {{ text }}
-           </p>
-           <h3>
-            {{ author }}
-           </h3>
-        </div>
-           
-       </div>
- </section>
+    </section>
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/partials/variables.scss" as *;
 
 .container {
-    padding-top: 140px;
-    height: 945px;
+    height: 755px;
     display: flex;
-    
 
-    img {
-        
-        height: 900px;
-        max-width: 1090px;
+    .container-img {
+        width: 165%;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+
+    .title-position {
+        margin-top: 130px;
+
+        p {
+            padding: 15px;
+            font-size: 23px;
+            margin-top: 30px;
+            width: 60%;
+            margin-left: 55px;
+        }
+
 
     }
-    .title-position{
-       p{
-        padding: 15px;
-        font-size: 23px;
-       }
-       h3{
-        padding: 15px;
-        font-size: 2rem;
-       }
-    }
-   
-    
-
-
-
-
 
     h2 {
-        font-size: 7rem;
+        font-size: 5.5rem;
         position: relative;
-        right: 92px;
-      
+        right: 50px;
+
+
     }
 }
 
-@media all and (max-width: 1250px) {
-   
-   
-}
+@media all and (max-width: 1250px) {}
 
-@media all and (max-width: 1000px) {
-   
-}
+@media all and (max-width: 1000px) {}
 
 @media all and (max-width: 900px) {}
 </style>
